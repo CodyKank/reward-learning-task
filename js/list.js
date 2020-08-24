323cf6743a66e4d7da2311b3e93e76a4fc6e0735
CodyKank <cody.kankel@gmail.com>
2020-08-24 14:25:57 -0400

    Change participant id's

--- js/list.js
+++ js/list.js
@@ -23,7 +23,7 @@ picArray[110] = ["images/14.png",
   "images/101.png",
   "images/102.png"
 ];  
-picArray[034] = ["images/14.png",
+picArray[134] = ["images/14.png",
   "images/18.png",
   "images/31.png",
   "images/37.png",
@@ -34,7 +34,7 @@ picArray[034] = ["images/14.png",
   "images/101.png",
   "images/102.png"
 ];  
-picArray[921832] = ["images/14.png",
+picArray[921] = ["images/14.png",
   "images/18.png",
   "images/31.png",
   "images/37.png",
@@ -45,7 +45,7 @@ picArray[921832] = ["images/14.png",
   "images/101.png",
   "images/102.png"
 ];  
-picArray[753642] = ["images/14.png",
+picArray[753] = ["images/14.png",
   "images/18.png",
   "images/31.png",
   "images/37.png",
@@ -58,7 +58,7 @@ picArray[753642] = ["images/14.png",
 ];  
 subjcodeArray[111] = "kindpeople";
 subjcodeArray[110] = "fuzzynarwal";
-subjcodeArray[034] = "purplebucket";
-subjcodeArray[921832] = "bluedinosaur";
-subjcodeArray[753642] = "magicconch";
+subjcodeArray[134] = "purplebucket";
+subjcodeArray[921] = "bluedinosaur";
+subjcodeArray[753] = "magicconch";
 
