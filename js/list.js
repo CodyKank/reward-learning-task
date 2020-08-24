6be19f9b95fa1e73ce167a1d78289b61a891c100
CodyKank <cody.kankel@gmail.com>
2020-08-24 11:48:42 -0400

    Add testing hardcoded participants in list.js
    First test of adding additional particpants is added.
    Large sequence of numbers initially used, random words
    used for "secret code".

--- js/list.js
+++ js/list.js
@@ -12,4 +12,41 @@ picArray[111] = ["images/14.png",
   "images/101.png",
   "images/102.png"
 ];  
+picArray[401502] = ["images/14.png",
+  "images/18.png",
+  "images/31.png",
+  "images/37.png",
+  "images/28.png",
+  "images/11.png",
+  "images/5.png",
+  "images/13.png",
+  "images/101.png",
+  "images/102.png"
+];  
+picArray[921832] = ["images/14.png",
+  "images/18.png",
+  "images/31.png",
+  "images/37.png",
+  "images/28.png",
+  "images/11.png",
+  "images/5.png",
+  "images/13.png",
+  "images/101.png",
+  "images/102.png"
+];  
+picArray[753642] = ["images/14.png",
+  "images/18.png",
+  "images/31.png",
+  "images/37.png",
+  "images/28.png",
+  "images/11.png",
+  "images/5.png",
+  "images/13.png",
+  "images/101.png",
+  "images/102.png"
+];  
 subjcodeArray[111] = "kindpeople";
+subjcodeArray[401502] = "purplebucket";
+subjcodeArray[921832] = "bluedinosaur";
+subjcodeArray[753642] = "magicconch";
+
